adb38647c9db81599759acedef2e92e71e3df31e
Kitty Hsieh <kitty40632@gmail.com>
2022-09-07 20:04:27 +0800

    feat(jobs,-users): modify Job models

--- src/app/jobs/models.py
+++ src/app/jobs/models.py
@@ -1,27 +1,31 @@
 from django.db import models
 
+from app.users.models import User
 
-class Company(models.Model):
-    name = models.CharField(unique=True, max_length=255)
+
+class Job(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
 
 
-class Job(models.Model):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+class Wage(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
     hourly_rate = models.PositiveIntegerField(default=0)
+    start_date = models.DateField()
+    end_date = models.DateField()
 
     def __str__(self) -> str:
-        return f"{self.company.name}: {self.user.username}"
+        return f"{self.hourly_rate}"
 
 
 class WorkDay(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    wage = models.ForeignKey(Wage, on_delete=models.CASCADE)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
     hours = models.PositiveIntegerField(default=0)
 
     def __str__(self) -> str:
-        return f"{self.job}"
+        return f"{self.wage}: {self.hours}"
--- src/app/users/models.py
+++ src/app/users/models.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-from app.jobs.models import Company
 
 
 class User(AbstractUser):
-    company = models.ManyToManyField(Company, through="jobs.Job")
+    pass
